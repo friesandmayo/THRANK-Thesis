095e30390731801cc2d3e8fadaacc56a565259f5
friesandmayo <friesandmayo1.0@gmail.com>
2026-06-04 09:36:11 +0200

    update p4

--- sensitivity_analysis/theta_sensitivity/+model_sensitivity/+sparse/+block/static_8.m
+++ sensitivity_analysis/theta_sensitivity/+model_sensitivity/+sparse/+block/static_8.m
@@ -1,9 +1,9 @@
 function [y, T, residual, g1] = static_8(y, x, params, sparse_rowval, sparse_colval, sparse_colptr, T)
 residual=NaN(1, 1);
-  residual(1)=(y(58))-(y(57)+params(4)*y(58));
+  residual(1)=(y(61))-(y(60)+params(5)*y(61));
 if nargout > 3
     g1_v = NaN(1, 1);
-g1_v(1)=1-params(4);
+g1_v(1)=1-params(5);
     if ~isoctave && matlab_ver_less_than('9.8')
         sparse_rowval = double(sparse_rowval);
         sparse_colval = double(sparse_colval);
--- sensitivity_analysis/theta_sensitivity/+model_sensitivity/dynamic_params_derivs.m
+++ sensitivity_analysis/theta_sensitivity/+model_sensitivity/dynamic_params_derivs.m
@@ -52,8 +52,8 @@ function [rp, gp, rpp, gpp, hp, g3p] = dynamic_params_derivs(y, x, params, stead
 %           from model file (.mod)
 
 T = NaN(0,1);
-rp = zeros(75, 39);
-gp = zeros(75, 118, 39);
+rp = zeros(74, 40);
+gp = zeros(74, 117, 40);
 if nargout >= 3
 rpp = zeros(0,4);
 gpp = zeros(0,5);
